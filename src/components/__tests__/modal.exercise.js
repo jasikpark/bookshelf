185c3104138777b2c52e83cfd3ffbccc678fb78e
Caleb Jasik <calebjasik@jasik.xyz>
2022-07-20 07:54:45 -0500

    Test the modal bits!

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,11 +1,36 @@
-// 🐨 you're gonna need this stuff:
-// import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import * as React from 'react'
+import {render, screen, within} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Modal, ModalOpenButton, ModalContents} from '../modal'
 
-test.todo('can be opened and closed')
-// 🐨 render the Modal, ModalOpenButton, and ModalContents
-// 🐨 click the open button
-// 🐨 verify the modal contains the modal contents, title, and label
-// 🐨 click the close button
-// 🐨 verify the modal is no longer rendered
-// 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
-// 💰 Remember all userEvent utils are async, so you need to await them.
+test('can be opened and closed', async () => {
+  const label = 'Modal label'
+  const title = 'Modal title'
+  // 🐨 render the Modal, ModalOpenButton, and ModalContents
+  render(
+    <Modal>
+      <ModalOpenButton>
+        <button>Open</button>
+      </ModalOpenButton>
+      <ModalContents title={title} aria-label={label}>
+        Hello World
+      </ModalContents>
+    </Modal>,
+  )
+  // 🐨 click the open button
+  await userEvent.click(screen.getByRole('button', {name: /open/i}))
+  // 🐨 verify the modal contains the modal contents, title, and label
+  const modal = screen.getByRole('dialog', {name: label})
+  const {getByRole, getByText} = within(modal)
+
+  expect(getByRole('heading', {name: title})).toBeInTheDocument()
+  expect(getByText('Hello World')).toBeInTheDocument()
+  // 🐨 click the close button
+  await userEvent.click(getByRole('button', {name: /close/i}))
+  // 🐨 verify the modal is no longer rendered
+  // 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
+  // 💰 Remember all userEvent utils are async, so you need to await them.
+  await expect(
+    screen.queryByRole('dialog', {name: title}),
+  ).not.toBeInTheDocument()
+})
