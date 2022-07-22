f5037b05127ad4f0ac4c36eb9ee8eb66229d82e5
Caleb Jasik <calebjasik@jasik.xyz>
2022-07-22 13:30:59 -0500

    Use the nice and pretty msw and databases

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -11,58 +11,36 @@ import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
 import {AppProviders} from 'context'
 import {App} from 'app'
+import {server} from 'test/server'
 
-// 🐨 after each test, clear the queryCache and auth.logout
+import * as usersDB from 'test/data/users'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
 afterEach(() => {
   queryCache.clear()
   auth.logout()
 })
 
 test('renders all the book information', async () => {
-  // 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
-  window.localStorage.setItem(auth.localStorageKey, 'SOME_FAKE_TOKEN')
-  // 🐨 create a user using `buildUser`
   const user = buildUser()
-  // 🐨 create a book use `buildBook`
-  const book = buildBook()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  // this is what our auth provider does to persist the user's
+  // logged in state so it can give us a token without making a request
+  // every provider will be different and you'll need to adjust this
+  // to whatever they do (you may even have to mock more of their functions)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
+
   // 🐨 update the URL to `/book/${book.id}`
   //   💰 window.history.pushState({}, 'page title', route)
   //   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
   window.history.pushState({}, 'Book Details', `/book/${book.id}`)
-  // 🐨 reassign window.fetch to another function and handle the following requests:
-  // - url ends with `/bootstrap`: respond with {user, listItems: []}
-  // - url ends with `/list-items`: respond with {listItems: []}
-  // - url ends with `/books/${book.id}`: respond with {book}
-  // 💰 window.fetch = async (url, config) => { /* handle stuff here*/ }
-  // 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
-  window.fetch = async (url, config) => {
-    if (url.endsWith('/bootstrap')) {
-      return Promise.resolve({
-        ok: true,
-        json: async () => ({
-          user,
-          listItems: [],
-        }),
-      })
-    }
-    if (url.endsWith('/list-items')) {
-      return Promise.resolve({
-        ok: true,
-        json: async () => ({
-          listItems: [],
-        }),
-      })
-    }
-    if (url.endsWith(`/books/${book.id}`)) {
-      return Promise.resolve({
-        ok: true,
-        json: async () => ({
-          book,
-        }),
-      })
-    }
-    throw new Error('your test is weird')
-  }
+
   // 🐨 render the App component and set the wrapper to the AppProviders
   // (that way, all the same providers we have in the app will be available in our tests)
   render(<App />, {wrapper: AppProviders})
@@ -77,4 +55,8 @@ test('renders all the book information', async () => {
   expect(screen.getByText(book.author)).toBeInTheDocument()
   expect(screen.getByText(book.publisher)).toBeInTheDocument()
   expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+
+  await usersDB.reset()
+  await booksDB.reset()
+  await listItemsDB.reset()
 })
