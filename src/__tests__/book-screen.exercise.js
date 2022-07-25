532b7dc2301fa550c331a47189fd7c7ee75b10e6
Caleb Jasik <calebjasik@jasik.xyz>
2022-07-25 06:19:24 -0500

    Move test utilities to app-test-utils and setupTests

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,91 +1,34 @@
 // 🐨 here are the things you're going to need for this test:
 import * as React from 'react'
-import {
-  findByText,
-  render as rtlRender,
-  screen,
-  waitForElementToBeRemoved,
-} from '@testing-library/react'
+import {screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import {queryCache} from 'react-query'
-import {buildUser, buildBook} from 'test/generate'
-import * as auth from 'auth-provider'
-import {AppProviders} from 'context'
-import {App} from 'app'
-import {server} from 'test/server'
-
-import * as usersDB from 'test/data/users'
+import {buildBook} from 'test/generate'
+import {render, waitForLoadingToFinish, loginAsUser} from 'test/app-test-utils'
 import * as booksDB from 'test/data/books'
-import * as listItemsDB from 'test/data/list-items'
-
-const waitForLoadingToFinish = async () =>
-  await waitForElementToBeRemoved(() => [
-    ...screen.queryAllByLabelText(/loading/i),
-    ...screen.queryAllByText(/loading/i),
-  ])
-
-const loginAsUser = async () => {
-  const user = buildUser()
-  await usersDB.create(user)
-  const authUser = await usersDB.authenticate(user)
-  // this is what our auth provider does to persist the user's
-  // logged in state so it can give us a token without making a request
-  // every provider will be different and you'll need to adjust this
-  // to whatever they do (you may even have to mock more of their functions)
-  window.localStorage.setItem(auth.localStorageKey, authUser.token)
-}
-
-const render = () => rtlRender(<App />, {wrapper: AppProviders})
-
-beforeAll(() => server.listen())
-afterAll(() => server.close())
-afterEach(() => {
-  queryCache.clear()
-  auth.logout()
-})
+import {App} from 'app'
 
 test('renders all the book information', async () => {
   await loginAsUser()
-
   const book = await booksDB.create(buildBook())
-
-  // 🐨 update the URL to `/book/${book.id}`
-  //   💰 window.history.pushState({}, 'page title', route)
-  //   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
   window.history.pushState({}, 'Book Details', `/book/${book.id}`)
 
-  // 🐨 render the App component and set the wrapper to the AppProviders
-  // (that way, all the same providers we have in the app will be available in our tests)
-  render()
-  // 🐨 use findBy to wait for the book title to appear
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#findby-queries
+  render(<App />)
+
   await waitForLoadingToFinish()
   // 🐨 assert the book's info is in the document
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
   expect(screen.getByText(book.author)).toBeInTheDocument()
   expect(screen.getByText(book.publisher)).toBeInTheDocument()
   expect(screen.getByText(book.synopsis)).toBeInTheDocument()
-
-  await usersDB.reset()
-  await booksDB.reset()
-  await listItemsDB.reset()
 })
 
 test('can create a list item for the book', async () => {
   await loginAsUser()
-
   const book = await booksDB.create(buildBook())
-
-  // 🐨 update the URL to `/book/${book.id}`
-  //   💰 window.history.pushState({}, 'page title', route)
-  //   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
   window.history.pushState({}, 'Book Details', `/book/${book.id}`)
 
-  // 🐨 render the App component and set the wrapper to the AppProviders
-  // (that way, all the same providers we have in the app will be available in our tests)
-  render()
-  // 🐨 use findBy to wait for the book title to appear
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#findby-queries
+  render(<App />)
+
   await waitForLoadingToFinish()
   // 🐨 assert the book's info is in the document
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
@@ -103,8 +46,4 @@ test('can create a list item for the book', async () => {
   expect(
     screen.getByRole('button', {name: /remove from list/i}),
   ).toBeInTheDocument()
-
-  await usersDB.reset()
-  await booksDB.reset()
-  await listItemsDB.reset()
 })
---
+++ src/test/app-test-utils.exercise.js
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import {
+  waitForElementToBeRemoved,
+  screen,
+  render as rtlRender,
+} from '@testing-library/react'
+import {buildUser} from 'test/generate'
+import * as auth from 'auth-provider'
+import {AppProviders} from 'context'
+
+import * as usersDB from 'test/data/users'
+
+export const waitForLoadingToFinish = async () =>
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
+
+export const loginAsUser = async () => {
+  const user = buildUser()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  // this is what our auth provider does to persist the user's
+  // logged in state so it can give us a token without making a request
+  // every provider will be different and you'll need to adjust this
+  // to whatever they do (you may even have to mock more of their functions)
+  window.localStorage.setItem(auth.localStorageKey, authUser.token)
+}
+
+export const render = (ui, options) =>
+  rtlRender(ui, {wrapper: AppProviders, ...options})
