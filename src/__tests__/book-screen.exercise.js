d789b56f1fff4f9d574ef5cc7ed5aa5355ddd200
Caleb Jasik <calebjasik@jasik.xyz>
2022-07-26 04:41:55 -0500

    Make sure we don't have a finish date on our unread book

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -11,16 +11,33 @@ import {buildBook, buildListItem} from 'test/generate'
 import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
 import {App} from 'app'
+import {server, rest} from 'test/server'
 
 const fakeTimerUserEvent = userEvent.setup({
   advanceTimers: () => jest.runOnlyPendingTimers(),
 })
 
-test('renders all the book information', async () => {
-  const book = await booksDB.create(buildBook())
+async function renderBookScreen({user, book, listItem} = {}) {
+  user = typeof user === 'undefined' ? await loginAsUser() : user
+  book = typeof book === 'undefined' ? await booksDB.create(buildBook()) : book
+
+  listItem =
+    typeof listItem === 'undefined'
+      ? await listItemsDB.create(buildListItem({owner: user, book}))
+      : listItem
   const route = `/book/${book.id}`
 
-  await render(<App />, {route})
+  const result = await render(<App />, {route, user})
+
+  return {
+    book,
+    listItem,
+    ...result,
+  }
+}
+
+test('renders all the book information', async () => {
+  const {book} = await renderBookScreen({listItem: null})
 
   // 🐨 assert the book's info is in the document
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
@@ -30,9 +47,7 @@ test('renders all the book information', async () => {
 })
 
 test('can create a list item for the book', async () => {
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-  await render(<App />, {route})
+  const {book} = await renderBookScreen({listItem: null})
 
   // 🐨 assert the book's info is in the document
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
@@ -54,12 +69,7 @@ test('can create a list item for the book', async () => {
 
 test('can remove a list item for the book', async () => {
   // prepare
-  const user = await loginAsUser()
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-  await listItemsDB.create(buildListItem({owner: user, book}))
-  await render(<App />, {route, user})
-
+  const {book} = await renderBookScreen()
   // end prepare
 
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
@@ -72,13 +82,8 @@ test('can remove a list item for the book', async () => {
 })
 
 test('can mark a list item as read', async () => {
-  // prepare
-  const user = await loginAsUser()
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-  await listItemsDB.create(buildListItem({owner: user, book, finishDate: null}))
-  await render(<App />, {route, user})
-  // end prepared
+  const {book, listItem} = await renderBookScreen()
+  await listItemsDB.update(listItem.id, {finishDate: null})
 
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
 
@@ -94,14 +99,7 @@ test('can mark a list item as read', async () => {
 test('can edit a note', async () => {
   // using fake timers to skip debounce time
   jest.useFakeTimers()
-  const user = await loginAsUser()
-  const book = await booksDB.create(buildBook())
-  const listItem = await listItemsDB.create(buildListItem({owner: user, book}))
-  const route = `/book/${book.id}`
-
-  await render(<App />, {route, user})
-
-  //   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+  const {listItem} = await renderBookScreen()
 
   const newNotes = faker.lorem.words()
   const notesTextarea = screen.getByRole('textbox', {name: /notes/i})
@@ -120,3 +118,44 @@ test('can edit a note', async () => {
     notes: newNotes,
   })
 }, 5000)
+
+test('shows an error message when the book fails to load', async () => {
+  const book = await booksDB.create(buildBook({id: 1234567890}))
+  await renderBookScreen({book, listItem: null})
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"There was an error: Book not found"`,
+  )
+})
+
+test('note update failures are displayed', async () => {
+  const apiURL = process.env.REACT_APP_API_URL
+
+  const testErrorMessage = '__test_error_message__'
+  server.use(
+    rest.put(`${apiURL}/list-items/:listItemId`, async (req, res, ctx) => {
+      return res(
+        ctx.status(400),
+        ctx.json({status: 400, message: testErrorMessage}),
+      )
+    }),
+  )
+
+  // using fake timers to skip debounce time
+  jest.useFakeTimers()
+  await renderBookScreen()
+
+  const newNotes = faker.lorem.words()
+  const notesTextarea = screen.getByRole('textbox', {name: /notes/i})
+
+  await fakeTimerUserEvent.clear(notesTextarea)
+  await fakeTimerUserEvent.type(notesTextarea, newNotes)
+
+  // wait for the loading spinner to show up
+  await screen.findByLabelText(/loading/i)
+  // wait for the loading spinner to go away
+  await waitForLoadingToFinish()
+
+  expect(screen.getByRole('alert')).toHaveTextContent(
+    `There was an error: ${testErrorMessage}`,
+  )
+})
